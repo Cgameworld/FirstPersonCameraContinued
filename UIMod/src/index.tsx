8c95109942354334426067f303327597e4113e0d
Cgameworld <support@cgameworld.com>
2024-04-21 14:54:46 -0700

    button outputs currententity, log cleanup

--- UIMod/src/index.tsx
+++ UIMod/src/index.tsx
@@ -13,7 +13,7 @@ const register: ModRegistrar = (moduleRegistry) => {
         </div>;
     }
 
-    //listen and inject the item into the DOM manually, I don't understand how to put the button in the same row in the official UI system
+    //listen and inject the item into the DOM manually, can't figure out how to put the button in the same row in the official UI system
     const observeAndAppend = (): void => {
         const targetNode: HTMLElement | null = document.querySelector('.info-layout_BVk');
         const config: MutationObserverInit = { childList: true, subtree: true };
@@ -22,15 +22,15 @@ const register: ModRegistrar = (moduleRegistry) => {
                 if (mutation.type === 'childList') {
                     let element: HTMLElement | null = document.querySelector('.actions-section_X1x');
                     if (element && !element.querySelector('div')) {
-                        console.log('Element .actions-section_X1x found:', element);
+                        //console.log('Element .actions-section_X1x found:', element);
                         let div: HTMLDivElement = document.createElement('div');
-                        div.innerHTML = `<button class="ok button_Z9O button_ECf item_It6 item-mouse-states_Fmi item-selected_tAM item-focused_FuT button_Z9O button_ECf item_It6 item-mouse-states_Fmi item-selected_tAM item-focused_FuT button_xGY">
+                        div.innerHTML = `<button class="ok button_Z9O button_ECf item_It6 item-mouse-states_Fmi item-selected_tAM item-focused_FuT button_Z9O button_ECf item_It6 item-mouse-states_Fmi item-selected_tAM item-focused_FuT button_xGY" onClick="console.log(\`${currentEntity}\`)">
     <img class="icon_Tdt icon_soN icon_Iwk" src="coui://uil/Colored/ArrowLeft.svg"></img>
 </button>`;
                         element.appendChild(div);
-                        console.log('New div appended:', div);
+                        //console.log('New div appended:', div);
                         observer.disconnect();
-                        console.log('Observer disconnected');
+                        //console.log('Observer disconnected');
                         break;
                     }
                 }
@@ -39,17 +39,14 @@ const register: ModRegistrar = (moduleRegistry) => {
 
         let observer: MutationObserver = new MutationObserver(callback);
 
-        console.log('Starting to observe:', targetNode);
         if (targetNode) {
             observer.observe(targetNode, config);
-        } else {
-            console.log('Target node not found');
         }
     }
 
     moduleRegistry.append('GameTopRight', CustomMenuButton);
 
-    //keep track of current selected entity
+    //keep track of current selected entity - from plop the growables
     const selectedEntity$ = selectedInfo.selectedEntity$;
     let currentEntity: any = null;
 
@@ -61,7 +58,6 @@ const register: ModRegistrar = (moduleRegistry) => {
         if (currentEntity != entity.index) {
             currentEntity = entity.index
         }
-        console.log("new entity: " + currentEntity);
         observeAndAppend();
         return entity;
     })
