44e7d5507ad3134642fc4fb46643f1252ed17f34
Cgameworld <support@cgameworld.com>
2024-04-28 15:12:08 -0700

    filter button injection out of specfic infoviews

--- UIMod/src/index.tsx
+++ UIMod/src/index.tsx
@@ -16,6 +16,7 @@ const register: ModRegistrar = (moduleRegistry) => {
     moduleRegistry.append('GameTopRight', CustomMenuButton);
 
   
+    const middleSections$ = selectedInfo.middleSections$;
 
     //listen and inject the item into the DOM manually, can't figure out how to put the button in the same row in the official UI system
     const observeAndAppend = (): void => {
@@ -25,8 +26,12 @@ const register: ModRegistrar = (moduleRegistry) => {
             for (let mutation of mutationsList) {
                 if (mutation.type === 'childList') {
                     let element: HTMLElement | null = document.querySelector('.actions-section_X1x');
-                    if (element && !element.querySelector('div')) {
-                        //console.log('Element .actions-section_X1x found:', element);
+                    if (element && !element.querySelector('div') && !middleSections$.value.some(x =>
+                            x?.__Type === "Game.UI.InGame.LevelSection" as any ||
+                            x?.__Type === "Game.UI.InGame.RoadSection" as any ||
+                            x?.__Type === "Game.UI.InGame.ResidentsSection" as any
+                        )) {
+                        console.log('Element .actions-section_X1x found:', element);
                         let div: HTMLDivElement = document.createElement('div');
                         div.innerHTML = `<button style="margin-left:5rem;margin-right:8rem" class="ok button_Z9O button_ECf item_It6 item-mouse-states_Fmi item-selected_tAM item-focused_FuT button_Z9O button_ECf item_It6 item-mouse-states_Fmi item-selected_tAM item-focused_FuT button_xGY">
     <img class="icon_Tdt icon_soN icon_Iwk" src="coui://uil/Colored/VideoCamera.svg"></img>
