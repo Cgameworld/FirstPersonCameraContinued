49ed5d062e99595f5242b6f0719e7ce0d7cc56ba
Cgameworld <support@cgameworld.com>
2026-02-26 12:57:09 -0800

    fix for lag looking down

--- Patches/CameraUpdateSystemPatch.cs
+++ Patches/CameraUpdateSystemPatch.cs
@@ -15,6 +15,11 @@ namespace FirstPersonCameraContinued.Patches
         {
             var camera = __instance.World.GetExistingSystemManaged<FirstPersonCameraSystem>();
             camera?.UpdateCamera();
+
+            if (camera != null && camera.EntryInfo != null && camera.EntryInfo.Activated && __instance.activeViewer != null)
+            {
+                __instance.activeViewer.shadowsAdjustFarDistance = false;
+            }
         }
 
         static void Postfix(CameraUpdateSystem __instance, ref DepthOfField ___m_DepthOfField)
@@ -23,6 +28,10 @@ namespace FirstPersonCameraContinued.Patches
             if (camera != null && camera.EntryInfo != null && camera.EntryInfo.Activated)
             {
                 ___m_DepthOfField.focusMode.Override(DepthOfFieldMode.Off);
+            }
+            else if (__instance.activeViewer != null)
+            {
+                __instance.activeViewer.shadowsAdjustFarDistance = true;
             }
         }
     }
